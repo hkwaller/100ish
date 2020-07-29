518f7813905cd65ad201e589145319594e36cee1
Hannes Waller <hkwaller@gmail.com>
2020-07-29 20:45:13 +0200

    add cool haptics

--- ios/hundredish/AppDelegate.mm
+++ ios/hundredish/AppDelegate.mm
@@ -16,6 +16,9 @@
 #import <React/JSCExecutorFactory.h>
 #import <RNReanimated/RETurboModuleProvider.h>
 
+#import <UMCore/UMModuleRegistry.h>
+#import <UMReactNativeAdapter/UMNativeModulesProxy.h>
+#import <UMReactNativeAdapter/UMModuleRegistryAdapter.h>
   
 #if DEBUG
 #import <FlipperKit/FlipperClient.h>
@@ -38,9 +41,13 @@ static void InitializeFlipper(UIApplication *application) {
 }
 #endif
 
+
 @interface AppDelegate() <RCTCxxBridgeDelegate, RCTTurboModuleManagerDelegate> {
     RCTTurboModuleManager *_turboModuleManager;
 }
+
+@property (nonatomic, strong) UMModuleRegistryAdapter *moduleRegistryAdapter;
+
 @end
 
 @implementation AppDelegate
@@ -51,6 +58,7 @@ static void InitializeFlipper(UIApplication *application) {
 #if DEBUG
   InitializeFlipper(application);
 #endif
+self.moduleRegistryAdapter = [[UMModuleRegistryAdapter alloc] initWithModuleRegistryProvider:[[UMModuleRegistryProvider alloc] init]];
 
 //  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
 //  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
@@ -78,6 +86,13 @@ static void InitializeFlipper(UIApplication *application) {
   RCTEnableTurboModule(YES);
   
   return YES;
+}
+
+- (NSArray<id<RCTBridgeModule>> *)extraModulesForBridge:(RCTBridge *)bridge
+{
+    NSArray<id<RCTBridgeModule>> *extraModules = [_moduleRegistryAdapter extraModulesForBridge:bridge];
+    // If you'd like to export some custom RCTBridgeModules that are not Expo modules, add them here!
+    return extraModules;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
